c833518b5dde2919547be9ef3f36ee4a623c1cff
Prasanth Sukumar <sukumar@outlook.ie>
2022-03-16 20:47:29 +0000

    Added python code

--- analysis.py
+++ analysis.py
@@ -1,2 +1,15 @@
 #Analyis project
 #Author : Prasanth Sukumar
+
+import pandas as pd
+
+df = pd.read_csv('iris.data', header=None)
+print(df)
+print(df.dtypes)
+print(df.columns)
+
+df=df.rename(columns={df.columns[0]: "sepal_length_cm", df.columns[1]: "sepal_width_cm",df.columns[2]: "petal_length_cm", df.columns[3]: "petal_width_cm", df.columns[4]: "class"})
+print(df.columns)
+print(df)
+
+print(df['class'].value_counts())
